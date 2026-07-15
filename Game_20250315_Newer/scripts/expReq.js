ced3da89e0c3427c87415a0941bfe7b6b9e03035
matthewssa <steven.matthews86@gmail.com>
2026-07-15 07:51:59 -0400

    Delete Game_20250315_Newer directory

--- Game_20250315_Newer/scripts/expReq.js
+++
@@ -1,8 +0,0 @@
-// Experience requirements data.
-// No direct character references in this file.
-// Define experience requirements for leveling up
-const expRequirements = [
-    0, 300, 900, 2700, 6500, 14000, 23000, 34000, 48000, 
-    64000, 85000, 100000, 120000, 140000, 165000, 195000, 
-    225000, 265000, 305000, 355000
-];
